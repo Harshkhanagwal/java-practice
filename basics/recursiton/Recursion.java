3eb7e3c81ed70e373b20e4ab5ddbc3bb4551ac12
harsh khanagwal <harshkhanagwal@harshs-MacBook-Air.local>
2026-04-25 12:04:50 +0530

    starting with oops

---
+++ basics/recursiton/Recursion.java
@@ -0,0 +1,15 @@
+package basics.recursiton;
+
+public class Recursion {
+    
+    // static void printN(int n);{
+    //     int i = 0;
+    //     while(i < n) {
+    //         System.out.print(i);
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        
+    }
+}
