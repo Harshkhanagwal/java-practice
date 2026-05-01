d44b2b036a45d904e17722c5113e28d481378bc5
harsh khanagwal <harshkhanagwall29@gmail.com>
2026-05-01 06:28:24 +0530

    abstraction example

--- OOps/oopsIntro/abstraction/Main.java
+++ OOps/oopsIntro/abstraction/Main.java
@@ -1,5 +1,35 @@
-package abstraction;
+abstract class Payment {
+    abstract void pay(double amount);  
+    void receipt(double amount) {
+        System.out.println("Payment of " + amount + " received");
+    }
+}
+
+class CreditCardPayment extends Payment {
+    @Override
+    void pay(double amount) {
+        System.out.println("Processing credit card payment of " + amount);
+    }
+}
+
+class UPIPayment extends Payment {
+    @Override
+    void pay(double amount) {
+        System.out.println("Processing UPI payment of " + amount);
+    }
+}
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Payment p;
+
+        p = new CreditCardPayment();
+        p.pay(1000);
+        p.receipt(1000);
+
+        p = new UPIPayment();
+        p.pay(500);
+        p.receipt(500);
+    }
 }
