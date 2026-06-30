adde7d0fecc1ad54e2f74221ebd4900fb135683a
harsh khanagwal <harshkhanagwall29@gmail.com>
2026-06-30 16:32:43 +0530

    deletion in linkedlst

--- basics/Linkedlist/LL.java
+++ basics/Linkedlist/LL.java
@@ -23,9 +23,11 @@ public class LL {
         Node curr = head;
 
         while (curr != null) {
-            System.err.println(curr.data);
+            System.out.print(curr.data + " -> ");
             curr = curr.next;
         }
+
+        System.out.println();
     }
 
     public void addFirst(String data) {
@@ -60,38 +62,77 @@ public class LL {
     }
 
     public void addAtIndex(String data, int indx) {
+
         Node newNode = new Node(data);
 
+        // Insert at beginning
         if (indx == 1) {
             newNode.next = head;
             head = newNode;
+            return;
         }
 
         Node tmp = head;
 
-        for (int i = 0; i < indx - 1; i++) {
+        // Move to the node before the desired index
+        for (int i = 1; i < indx - 1 && tmp != null; i++) {
             tmp = tmp.next;
         }
 
+        // Invalid index
         if (tmp == null) {
-            System.err.println("Invalid index");
+            System.out.println("Invalid Index");
             return;
         }
 
         newNode.next = tmp.next;
-        tmp = newNode;
+        tmp.next = newNode;
     }
 
+
+    public void deleteFirst() {
+        if(head == null) {
+            System.out.println("List is empty");
+        }else{
+            head = head.next;
+        }
+    }
+
+
+    public void deleteLast() {
+        if(head == null ) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if(head.next == null){
+            head = null;
+            return;
+        }
+
+        Node crnt = head;
+
+        while(crnt != null) {
+            if(crnt != null && crnt.next== null) {
+                crnt = null;
+            }
+            crnt = crnt.next;
+        }
+    }
     public static void main(String[] args) {
         LL list = new LL();
-        list.addFirst("hello");
-        list.addFirst("hello2");
-        list.addFirst("hello3");
-        list.addFirst("hello4");
-        list.addFirst("hello5");
-        list.addFirst("hello6");
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.addLast("4");
+        list.addLast("5");
+        list.addLast("6");
 
-        list.addLast("last elm");
+        list.addLast("7");
+
+        list.printlist();
+
+        list.deleteFirst();
 
         list.printlist();
     }
